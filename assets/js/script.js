2b6df25485ff00f27e6c78594f02b16c49bab353
Pat McDonald <patmcdonald@me.com>
2022-01-26 13:39:48 +0000

    initial set

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// taskie is a ToDo list app
+// Some basic functions for the app
+
+// define what a task is
+let task = document.querySelector()
+
+// store a tasks in an array
+let tasks = []
+
+// add a task
+function addTask()
+
+// display a task
+function displaytask()
+
+// store a task in taskie
+function storeTask()
+
+// edit task
+function editTask()
+
+// delete/remove task
+function deleteTask()
